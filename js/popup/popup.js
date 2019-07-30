c36d13d52de4d801b5dae439407645f6ec6907ab
Ioannis Papikas <papikas.ioan@gmail.com>
2019-07-30 19:36:09 +0300

    Fix selector for locale autodetect

--- js/popup/popup.js
+++ js/popup/popup.js
@@ -8,7 +8,7 @@
         });
 
         // Enable/Disable language
-        $(document).on('change, switchChange.bootstrapSwitch', '#integration-locale-auto-detect', function () {
+        $(document).on('change, switchChange.bootstrapSwitch', 'input[name="locale_autodetect"]', function () {
             checkIntegrationLocaleAutodetect();
         });
 
@@ -50,7 +50,7 @@
      * Enable/Disable language selector based on auto-detect option
      */
     function checkIntegrationLocaleAutodetect() {
-        $('#integration-locale-auto-detect').each(function () {
+        $('input[name="locale_autodetect"]').each(function () {
             let parent = $(this).closest('.questionnaire-override-container');
             let state = $(this).prop('checked');
             parent.find('.questionnaire-locale').toggleClass('disabled', state).prop("disabled", state);
